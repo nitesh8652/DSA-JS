842a52a82548b094fda626942dd5d9d01153ff4d
Primus <niteshsalian8652085233@gmail.com>
2026-08-25 17:45:29 +0530

    D4

---
+++ Loop/newReverse.js
@@ -0,0 +1,13 @@
+let prompt = require("prompt-sync")();
+let n = Number(prompt("Number : "));
+
+let rev = 0
+
+
+while (n > 0) {
+    let digit = n % 10
+    rev = rev * 10 + digit
+    n = Math.floor(n / 10)
+}
+
+console.log(rev)
