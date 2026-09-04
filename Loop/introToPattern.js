892b76dee66806f2f9fb0c47988072cc2ce52210
Primus <niteshsalian8652085233@gmail.com>
2026-09-04 18:19:21 +0530

    D9

--- Loop/introToPattern.js
+++ Loop/introToPattern.js
@@ -2,7 +2,7 @@ let prompt = require("prompt-sync")();
 let n = Number(prompt("Number : "))
 
 for(let i = 1; i<=n ;i++){
-    for(k=1;k<=n-i;k++){
+    for(k=1;k<=n;k++){
         process.stdout.write("*");
         
     }
---
+++ Loop/invertedTriangle.js
@@ -0,0 +1,12 @@
+let prompt = require("prompt-sync")();
+let n = Number(prompt("Number : "))
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i - 1; j++) {
+        process.stdout.write(" ")
+    }
+    for (let j = 1; j <= n - i + 1; j++) {
+        process.stdout.write("*")
+    }
+    console.log()
+}
